5ebb780bc48db61fc9ce4d355dcf52caf4dfef8a
Pranit Yadav <42487480+Pranit46@users.noreply.github.com>
2021-12-23 20:33:07 +0530

    Update EditDamageCars.js

--- src/Components/EditDamageCars.js
+++ src/Components/EditDamageCars.js
@@ -2,42 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-  
+
 function EditDamageCars() {
   useEffect(() => {
-    
-      console.log(params);
-      if (params.id) {
-        console.log(params.id);
-        getData();
-      }  
+    console.log(params);
+    if (params.id) {
+      console.log(params.id);
+      getData();
+    }
   }, []);
 
   let navigate = useNavigate();
   const params = useParams();
-  let [name, setName] = useState("");
-  let [email, setEmail] = useState("");
-  let [number, setNumber] = useState("");
-  let [model, setModel] = useState("");
-  let [desc, setDesc] = useState("");
-
-  const dataValues = {
-    firstName: name,
-    emailReg: email,
-    modelName: model,
-    carNumber:number,
-    carDesc: desc,
-  }
+  // let [name, setName] = useState("");
+  // let [email, setEmail] = useState("");
+  // let [number, setNumber] = useState("");
+  // let [model, setModel] = useState("");
+  // let [desc, setDesc] = useState("");
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
-      dataValues
-      // firstName: name,
-      // emailReg: "",
-      // modelName: "",
-      // carNumber:"",
-      // carDesc:""
+      // dataValues
+      firstName: "",
+      emailReg: "",
+      modelName: "",
+      carNumber: "",
+      carDesc: "",
     },
     validationSchema: Yup.object({
       firstName: Yup.string()
@@ -56,9 +47,7 @@ function EditDamageCars() {
         .email("Invalid email address")
         .required("Required"),
 
-        carDesc: Yup.string()
-        .min(4, "Too short")
-        .required("Required"),
+      carDesc: Yup.string().min(4, "Too short").required("Required"),
     }),
     onSubmit: (values, onSubmitProps) => {
       handleSave(values);
@@ -66,45 +55,50 @@ function EditDamageCars() {
     },
   });
 
-  let handleSave = async(values)=>{
-    console.log("Roshan val" +formik.values.firstName);
-    await fetch('https://614eabfab4f6d30017b482c4.mockapi.io/cardetails/'+params.id,{
-        method:'PUT',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify({
-            name,
-            email,
-            number,
-            model,
-            desc
-        })
-    })
-    
-    .then(response =>response.json())
-    .then(data=>{
-        navigate('/all-cars')
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-}
+  let handleSave = async (values) => {
+    let dataToAdd = {
+      name: formik.values.firstName,
+      email: formik.values.emailReg,
+      number: formik.values.carNumber,
+      model: formik.values.modelName,
+      desc: formik.values.carDesc,
+    };
 
-  let getData = async()=>{
-    await fetch('https://614eabfab4f6d30017b482c4.mockapi.io/cardetails/'+params.id)
-    .then(response =>response.json())
-    .then(data=>{
-        setName(data.name)
-        setEmail(data.email)
-        setNumber(data.number)
-        setModel(data.model)
-        setDesc(data.desc)
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-}
+    await fetch(
+      "https://614eabfab4f6d30017b482c4.mockapi.io/cardetails/" + params.id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify( dataToAdd ),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        navigate("/all-cars");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  let getData = async () => {
+    await fetch(
+      "https://614eabfab4f6d30017b482c4.mockapi.io/cardetails/" + params.id
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        formik.setFieldValue("firstName", data.name);
+        formik.setFieldValue("emailReg", data.email);
+        formik.setFieldValue("modelName", data.model);
+        formik.setFieldValue("carNumber", data.number);
+        formik.setFieldValue("carDesc", data.desc);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
@@ -122,7 +116,7 @@ function EditDamageCars() {
             onBlur={formik.handleBlur}
             placeholder="Name"
           />
-           {formik.touched.firstName && formik.errors.firstName ? (
+          {formik.touched.firstName && formik.errors.firstName ? (
             <div style={{ color: "red" }}>{formik.errors.firstName}</div>
           ) : null}
         </div>
@@ -130,46 +124,70 @@ function EditDamageCars() {
           <label for="exampleInputEmail1">Email</label>
           <input
             type="email"
-            value={email}
+            value={formik.values.emailReg}
+            name="emailReg"
             className="form-control"
-            onChange={(e) => setEmail(e.target.value)}
+            // onChange={(e) => setEmail(e.target.value)}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="Enter email"
           />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
+          {formik.touched.emailReg && formik.errors.emailReg ? (
+            <div style={{ color: "red" }}>{formik.errors.emailReg}</div>
+          ) : null}
+
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Car number</label>
           <input
             type="text"
-            value={number}
+            name="carNumber"
+            value={formik.values.carNumber}
             className="form-control"
-            onChange={(e) => setNumber(e.target.value)}
+            // onChange={(e) => setNumber(e.target.value)}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="Enter car number"
           />
+          {formik.touched.carNumber && formik.errors.carNumber ? (
+            <div style={{ color: "red" }}>{formik.errors.carNumber}</div>
+          ) : null}
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Car Model</label>
           <input
             type="text"
-            value={model}
+            name="modelName"
+            value={formik.values.modelName}
             className="form-control"
-            onChange={(e) => setModel(e.target.value)}
+            // onChange={(e) => setModel(e.target.value)}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="Enter Car model"
           />
+          {formik.touched.modelName && formik.errors.modelName ? (
+            <div style={{ color: "red" }}>{formik.errors.modelName}</div>
+          ) : null}
         </div>
         <div className="form-group">
           <label for="exampleInputPassword1">Car Damage Description</label>
           <input
             type="text"
-            value={desc}
+            name="carDesc"
+            value={formik.values.carDesc}
             className="form-control"
-            onChange={(e) => setDesc(e.target.value)}
+            // onChange={(e) => setDesc(e.target.value)}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="Enter car damage description"
           />
+          {formik.touched.carDesc && formik.errors.carDesc ? (
+            <div style={{ color: "red" }}>{formik.errors.carDesc}</div>
+          ) : null}
         </div>
-        <button className="btn btn-primary" type="submit" onClick={handleSave}>
+        <button className="btn btn-success" type="submit" onClick={handleSave}
+        disabled={!formik.isValid || formik.isSubmitting}
+        >
           Submit
         </button>
       </form>
